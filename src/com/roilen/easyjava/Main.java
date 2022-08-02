976b84ead7200522249b2a590d2c2bec3d1e9e0a
Vadym <95472409+YaremchukVadym@users.noreply.github.com>
2022-08-02 12:58:17 +0300

    1

---
+++ src/com/roilen/easyjava/Main.java
@@ -0,0 +1,7 @@
+package com.roilen.easyjava;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/roilen/easyjava/classes/BaseClass.java
@@ -0,0 +1,11 @@
+package com.roilen.easyjava.classes;
+
+public interface BaseClass {
+    void attack();
+    void restoreHealth();
+    void loseHealth();
+    void restoreMana();
+    void upMana();
+    void levelUp();
+    void info();
+}
